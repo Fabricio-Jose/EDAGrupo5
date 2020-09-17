12a2ef199e3c70e3a0c5bb01caf8247a82513195
oscar-pfuturi-h <69215265+oscar-pfuturi-h@users.noreply.github.com>
2020-09-16 23:39:48 -0500

    Add files via upload

---
+++ Sorts/Merge/mergesort.py
@@ -0,0 +1,47 @@
+from random import randint, uniform,random
+
+def mergeSort(arr): 
+    if len(arr) >1: 
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+  
+        mergeSort(L)
+        mergeSort(R)
+  
+        i = j = k = 0
+
+        while i < len(L) and j < len(R): 
+            if L[i] < R[j]: 
+                arr[k] = L[i] 
+                i+= 1
+            else: 
+                arr[k] = R[j] 
+                j+= 1
+            k+= 1
+          
+        while i < len(L): 
+            arr[k] = L[i] 
+            i+= 1
+            k+= 1
+          
+        while j < len(R): 
+            arr[k] = R[j] 
+            j+= 1
+            k+= 1
+  
+def printList(arr): 
+    for i in range(len(arr)):         
+        print(arr[i], end =" ") 
+    print() 
+  
+if __name__ == '__main__': 
+    #n=int(input())
+    n=1000000
+    A=list(range(n))
+    for i in range(n):
+        A[i]=int(random()*n+1)
+    mergeSort(A) 
+    for i in range(n): 
+        print(A[i]), 
+  
