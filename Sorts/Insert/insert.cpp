020be05c5c6bb745f2a12355dabfcb5e1b1040eb
Thales <furion2298@gmail.com>
2020-09-21 16:59:09 -0500

    thales:automatizacion de la entrada de elementos en c++ y el git ignore

--- Sorts/Insert/insert.cpp
+++ Sorts/Insert/insert.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <time.h>
+
+bool verificar(int *A,int n)
+{
+    for(int i=1;i<n;++i)
+    {
+        if(A[i-1]>A[i])
+        {
+            return 0;
+        }
+        
+    }
+    return 1;
+}
+
 
 void insercion(int* A,int n)
 {
@@ -17,15 +33,42 @@ void insercion(int* A,int n)
 }
 int main()
 {
-	int tam = 100 ;
-	int *a = new int[tam];
-	for(int k=0;k<tam;k++) 
-	{
-        	std::cin>>a[k];            
-	} 
-	insercion(a, tam );
-	for(int i=0;i<tam;i++)
-        std::cout<<a[i]<<std::endl;
-	delete []a;
-	return 0;
+    int pruebas[]={10000,20000,30000,40000,50000};//colocar aqui los tamaños
+    double t0,t1,time;
+    for(auto x:pruebas)
+    {
+        std::cout<<x<<" ";
+        int *a=new int[x];
+        std::ifstream entrada("entrada.txt",std::ifstream::in);
+        for (int i=0;i<x;++i)
+        {
+            entrada>>a[i];
+            //std::cout<<a[i]<<std::endl;
+        }
+
+        //auto start = std::chrono::system_clock::now();
+        t0=clock();//Inicio del cronometro
+        insercion(a,x);
+        t1=clock();//FIn de cronometro
+        time = (double(t1-t0)/CLOCKS_PER_SEC);
+        std::cout <<time<< std::endl;
+
+
+
+        //auto end = std::chrono::system_clock::now();
+
+        //std::chrono::duration<double> elapsed = end - start;
+        //std::cout << "Elapsed time: " << elapsed.count() << "s";
+
+        
+        if(!verificar(a,x))
+        {
+            std::cout<<"ERROR en el ordenamiento"<<std::endl;
+        }
+        delete [] a;
+        entrada.close();
+    }
+    
+    
+    return 0;
 }
