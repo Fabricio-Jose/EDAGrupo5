1bcd3b1917f6187f08f2417714c2a00e2df39e2c
Fabichoz <zap.xtp@gmail.com>
2020-09-23 16:27:43 -0500

    Plot Quick Selection

---
+++ Sorts/Quick/QuickPlot.py
@@ -0,0 +1,17 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+x,y = np.loadtxt('QuickCpp.txt' , delimiter=',' , unpack=True)
+x1,y1 = np.loadtxt('QuickPy.txt' , delimiter=',' , unpack=True)
+x2,y2 = np.loadtxt('QuickJava.txt' , delimiter=',' , unpack=True)
+
+
+plt.plot(x,y,color="blue",label="Quick Sort C++")
+plt.plot(x1,y1,color="red",label="Quick Sort Python")
+plt.plot(x2,y2,color="yellow",label="Quick Sort Java")
+
+plt.legend()
+plt.title('Quick Sort Algorithm')
+plt.xlabel('Data')
+plt.ylabel('Seconds')
+plt.show()
---
+++ Sorts/Selection/SelectionPlot.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+#import cv2
+from matplotlib import pyplot as plt
+
+x,y = np.loadtxt('SelectionCpp.txt' , delimiter=',' , unpack=True)
+x1,y1 = np.loadtxt('SelectionPy.txt' , delimiter=',' , unpack=True)
+x2,y2 = np.loadtxt('SelectionJava.txt' , delimiter=',' , unpack=True)
+
+
+plt.plot(x,y,color="blue",label="Selection Sort C++")
+plt.plot(x1,y1,color="red",label="Selection Sort Python")
+plt.plot(x2,y2,color="yellow",label="Selection Sort Java")
+
+plt.legend()
+# plt.plot(s,d,color="red")
+plt.title('Algoritmos Selection')
+plt.xlabel('Datos')
+plt.ylabel('Segundos')
+plt.show()
